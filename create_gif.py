d404cca828d8ed486e70d15634fb5c7b723fb889
Thanuum <131961733+Thanuum@users.noreply.github.com>
2023-11-30 14:24:42 +0530

    Update create_gif.py

--- create_gif.py
+++ create_gif.py
@@ -8,4 +8,4 @@ images = [ ]
 for filename in filenames:
   images.append(imageio.imread(filename))
 
-imageio.mimsave('v.gif', images, duration=1000,loop=50)
+imageio.mimsave('t.gif', images, duration=1000,loop=50)
